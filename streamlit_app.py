b5b85f9c803d934936b95437c86b5ecc20afb1e9
Nguyen Thi Phuong Ngan <105268868+thatngn@users.noreply.github.com>
2025-01-09 23:26:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,35 @@
 import streamlit as st
+from langchain_ollama import ChatOllama
 
-st.title('🎈 App Name')
+# pip install -qU langchain-ollama
+# pip install langchain
 
-st.write('Hello world!')
+st.title("🧠 Make Your Own Chat Application with Ollama and Langchain!!!")
+
+# st.write("GET MORE INFO @ KGP Talkie: https://www.youtube.com/kgptalkie")
+
+with st.form("llm-form"):
+    text = st.text_area("Enter your question or statement:")
+    submit = st.form_submit_button("Submit")
+
+def generate_response(input_text):
+    model = ChatOllama(model="llama2:7b", base_url="http://localhost:11434/")
+
+    response = model.invoke(input_text)
+
+    return response.content
+
+if "chat_history" not in st.session_state:
+    st.session_state['chat_history'] = []
+
+if submit and text:
+    with st.spinner("Generating response..."):
+        response = generate_response(text)
+        st.session_state['chat_history'].append({"user": text, "ollama": response})
+        st.write(response)
+
+st.write("## Chat History")
+for chat in reversed(st.session_state['chat_history']):
+    st.write(f"**🧑 User**: {chat['user']}")
+    st.write(f"**🧠 Assistant**: {chat['ollama']}")
+    st.write("---")
