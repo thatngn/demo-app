42e52190525f3023e584b7f15ca5f8146eb2a2d0
Nguyen Thi Phuong Ngan <105268868+thatngn@users.noreply.github.com>
2025-01-10 20:03:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('Let's explore the AI assitant!', icon='️:sparkles:')
+        st.success('Let's explore the AI assitant!', icon='️✨')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
