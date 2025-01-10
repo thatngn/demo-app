aefa86b41379e99d4511cef1926e8da69d8dd54c
Nguyen Thi Phuong Ngan <105268868+thatngn@users.noreply.github.com>
2025-01-10 19:47:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,77 @@
 import streamlit as st
-from langchain_ollama import ChatOllama
+import replicate
+import os
 
-# pip install -qU langchain-ollama
-# pip install langchain
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-st.title("🧠 Make Your Own Chat Application with Ollama and Langchain!!!")
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-# st.write("GET MORE INFO @ KGP Talkie: https://www.youtube.com/kgptalkie")
+    st.subheader('Models and parameters')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'meta/llama-2-70b-chat'
+    elif selected_model == 'Llama2-13B':
+        llm = 'meta/llama-2-13b-chat'
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
 
-with st.form("llm-form"):
-    text = st.text_area("Enter your question or statement:")
-    submit = st.form_submit_button("Submit")
 
-def generate_response(input_text):
-    model = ChatOllama(model="llama2:7b", base_url="http://localhost:11434/")
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-    response = model.invoke(input_text)
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-    return response.content
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-if "chat_history" not in st.session_state:
-    st.session_state['chat_history'] = []
+# Function for generating LLaMA2 response. 
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
 
-if submit and text:
-    with st.spinner("Generating response..."):
-        response = generate_response(text)
-        st.session_state['chat_history'].append({"user": text, "ollama": response})
-        st.write(response)
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-st.write("## Chat History")
-for chat in reversed(st.session_state['chat_history']):
-    st.write(f"**🧑 User**: {chat['user']}")
-    st.write(f"**🧠 Assistant**: {chat['ollama']}")
-    st.write("---")
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
